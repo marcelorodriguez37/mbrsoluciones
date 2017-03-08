be395a9ded9032cefc0ff96187fb49e607547f3e
MARCELO RODRIGUEZ <rodriguez.m@hotmail.com.ar>
2017-03-07 21:59:19 -0300

    formulario de contacto terminado

--- contacto.php
+++ contacto.php
@@ -1,13 +1,19 @@
 <?php 
-print($POST['name']);
-die();
+$nombre=$_POST['name'];
+$tel=$_POST['phone'];
+$email=$_POST['email'];
+$mensaje=$_POST['message'];
+
 $to='info@mbrsoluciones.com';
 
-$subject='this is our test email';
+//$subject='this is our test email';
 
-$message='<h1> Es un prueba. </h1><p> veremos si funca</p>';
-$headers="From:The Sender Name <ro-milp@hotmail.com>\r\n";
-$headers.="Reply-To:ro-milp@hotmail.com>\r\n";
+$message=$mensaje;
+
+$headers="From:".$nombre.' '.$tel.' '.$email."\r\n";
+
+//$headers="From:The Sender Name<ro-milp@hotmail.com>\r\n";
+//$headers.="Reply-To:ro-milp@hotmail.com>\r\n";
 $headers.="Content-type: text/html\r\n";
 
 mail($to,$subject,$message,$headers);
